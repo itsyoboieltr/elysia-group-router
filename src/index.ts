f52a8b36bf5e17de2cfc3b7426604911dd5e4a67
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2023-07-28 01:30:43 +0200

    fix: give correct path in node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import type { Elysia } from 'elysia';
-import { join, dirname } from 'node:path';
+import path from 'node:path';
 import { glob } from 'glob';
 
 export default async function groupRouter(app: Elysia, groupsDir = 'groups') {
-  const groupsPath = join(import.meta.dir, groupsDir);
+  const groupsPath = path.join(path.dirname(Bun.main), groupsDir);
   const files = await glob('**/index.ts', { cwd: groupsPath, absolute: true });
   for (const file of files) {
     const { default: group } = await import(file);
-    const folderName = dirname(file).replace(groupsPath, '');
+    const folderName = path.dirname(file).replace(groupsPath, '');
     app.use((app) => group(app, folderName));
   }
   return app;
